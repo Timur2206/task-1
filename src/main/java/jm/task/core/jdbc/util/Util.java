ee2527449a97b4f29a5aeb28332336527b3fae4f
Timur Shamoyan <timurshamoyan.97@mail.ru>
2024-10-22 00:47:18 +0400

    [200~git commit -m

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,19 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
+     static  String userName = "postgres";
+     static  String password = "postgres";
+     static String connectionUrl="jdbc:postgresql://localhost:5432/users";
+    public static Connection open() {
+        try {
+            return DriverManager.getConnection(connectionUrl,userName,password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
     // реализуйте настройку соеденения с БД
 }
